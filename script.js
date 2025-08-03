4d4a6a185fb86763912e4012b09111d1d9fd4acb
amit-mndal <marchamit07@gmail.com>
2025-08-03 18:35:27 +0530

    Files Upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '0a4ae7e43amsh10e148215290358p1169b1jsnfb15fbc541fa',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
